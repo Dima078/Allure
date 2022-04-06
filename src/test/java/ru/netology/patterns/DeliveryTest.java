3c7a932608b5b55e78f07cb5358162050825eccc
Dmitry Kovtun <mr_write@mail.ru>
2022-04-06 09:41:36 +0300

    version9

--- src/test/java/ru/netology/patterns/DeliveryTest.java
+++ src/test/java/ru/netology/patterns/DeliveryTest.java
@@ -29,18 +29,18 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $x("//input[@type= 'text']").setValue(validUser.getCity());
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("//input[@type= 'tel']").val(firstMeetingDate);
-        $("[data-test-id=name] input").setValue(validUser.getName());
-        $("[data-test-id=phone] input").setValue(validUser.getPhone());
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id='success-notification'] .notification__title").should(visible, Duration.ofSeconds(4));
         $("[class='notification__content']")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + firstMeetingDate), Duration.ofSeconds(4));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("//input[@type= 'tel']").val(secondMeetingDate);
+        $("[data-test-id='date'] input").val(secondMeetingDate);
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .button").should(visible, Duration.ofSeconds(8)).click();
         $("[class='notification__content']")
